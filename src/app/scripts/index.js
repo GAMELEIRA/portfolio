d5e2673fcbfddd52d17346bce003d453bafc3bac
GAMELEIRA <gameleira270499@gmail.com>
2025-01-19 06:29:06 -0300

    refactor(academics-projects): Adiciona nova estrutura para exibicao dos projetos academicos

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -12,3 +12,46 @@ const observer = new IntersectionObserver(handleIntersection, {
 document.querySelectorAll('.card-experiences').forEach(card => {
   observer.observe(card);
 });
+
+
+async function loadProjects() {
+  const repos = [
+    "GAMELEIRA/studies-backend",
+    "GAMELEIRA/studies-database",
+    "GAMELEIRA/studies-frontend",
+    "GAMELEIRA/task-list-backend",
+    "GAMELEIRA/task-list-frontend",
+    "GAMELEIRA/portfolio",
+    "GAMELEIRA/rick-and-morty-app"
+  ];
+
+  const container = document.getElementById("list-academic-projects");
+
+  for (const repo of repos) {
+    const response = await fetch(`https://api.github.com/repos/${repo}`);
+    const data = await response.json();
+
+    console.log(data)
+
+    const card = document.createElement("div");
+    card.className = 'card-project';
+    card.innerHTML = `
+    <div id="card-project-image" class="center-content">
+      <img class="project-avatar" src="${data.owner?.avatar_url}" alt="Avatar do GitHub">
+    </div>
+    <div id="card-project-content" class="center-content">
+      <div>
+        <h2 class="headline headline-5 color-text-black card-project-title">${data.name}</h2>
+        <a href="${data.html_url}" target="_blank">
+          <p class="body body-1 color-text-black">${data.description}</p>
+          <p class="body body-1 color-text-black"><strong>Stars:</strong> ${data.stargazers_count} | <strong>Forks:</strong> ${data.forks_count}</p>       
+        </a>
+      </div>
+    </div>
+    </div>
+    `;
+    container.appendChild(card);
+  }
+}
+
+loadProjects();
